0870582ff1a66b26a474757414569afa2df2e9af
cyrill10 <cyrill.bader@gmx.ch>
2023-12-13 20:56:31 +0100

    remove old dependency

--- src/test/java/ch/bader/budget/boundary/AbstractIT.java
+++ src/test/java/ch/bader/budget/boundary/AbstractIT.java
@@ -4,23 +4,20 @@ import ch.bader.budget.TestUtils;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import net.sf.json.JSONSerializer;
+import org.bson.BsonArray;
 import org.bson.Document;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 
 abstract class AbstractIT {
 
-    protected void populateDatabaseFull(MongoClient mongoClient) throws IOException, URISyntaxException {
+    protected void populateDatabaseFull(final MongoClient mongoClient) throws IOException, URISyntaxException {
         populateMongoDbs(mongoClient);
     }
 
-    private void populateMongoDbs(MongoClient mongoClient) throws IOException, URISyntaxException {
+    private void populateMongoDbs(final MongoClient mongoClient) throws IOException, URISyntaxException {
         populateMongoDb(mongoClient, "virtualAccount");
         populateMongoDb(mongoClient, "realAccount");
         populateMongoDb(mongoClient, "transaction");
@@ -28,23 +25,18 @@ abstract class AbstractIT {
         populateMongoDb(mongoClient, "scannedTransaction");
     }
 
-    private void populateMongoDb(MongoClient mongoClient,
-                                 String collectionName) throws IOException, URISyntaxException {
-        String virtualAccountsString = TestUtils.loadFileAsString(
-            "dump/budget/2023-02-04_" + collectionName + ".txt");
+    private void populateMongoDb(final MongoClient mongoClient,
+                                 final String collectionName) throws IOException, URISyntaxException {
+        final String collectionString = TestUtils.loadFileAsString("dump/budget/2023-02-04_" + collectionName + ".txt");
 
-        MongoDatabase db = mongoClient.getDatabase("budget");
-        MongoCollection<Document> collection = db.getCollection(collectionName);
+        final MongoDatabase db = mongoClient.getDatabase("budget");
+        final MongoCollection<Document> collection = db.getCollection(collectionName);
         collection.deleteMany(new Document());
-        List<Document> jsonList = new ArrayList<>();
-        JSONArray array = JSONArray.fromObject(virtualAccountsString);
-        for (
-            Object object : array) {
-            JSONObject jsonStr = (JSONObject) JSONSerializer.toJSON(object);
-            Document jsnObject = Document.parse(jsonStr.toString());
-            jsonList.add(jsnObject);
 
-        }
+        final BsonArray bsonArray = BsonArray.parse(collectionString);
+
+        final List<Document> jsonList = bsonArray.stream().map(b -> Document.parse(b.toString())).toList();
+
         collection.insertMany(jsonList);
 
     }
